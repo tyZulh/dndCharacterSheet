8f8c07387f87ae9fa16227502670502cad68e3e8
Thomas Paoli <thomas@blueanchor.io>
2019-06-04 07:49:25 +0200

    remove duplicate code in the header components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,114 +1,27 @@
 import React, { Component } from 'react'
+import HeaderItem from '../headerItem/HeaderItem';
 import './Header.scss';
 
 class Header extends Component {
-  state = {
-    characterName: 'Einkil',
-    classes: '',
-    level: '',
-    age: '',
-    height: '',
-    weight: '',
-    gender: '',
-    edit: false,
-  }
 
-  editHeader = _=> {
-    this.setState({edit : !this.state.edit})
-  }
+  items = [
+    'Personnage',
+    'Classes',
+    'Niveau',
+    'Age',
+    'Taille',
+    'Poid',
+    'genre'
+  ]
 
   render() {
     return (
       <div className='header'>
         <div>
-          <div className='cell'>
-            <span>
-              Personnage : 
-            </span>
-
-            <span>
-              {this.state.characterName}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Classes : 
-            </span>
-
-            <span>
-              {this.state.classes} 
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Niveau : 
-            </span>
-            
-            <span>
-              {this.state.level}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Age : 
-            </span>
-            <span>
-              {this.state.age}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Taille : 
-            </span>
-
-            <span>
-              {this.state.height}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Poid :
-            </span>
-
-            <span>
-              {this.state.weight}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
-
-          <div className='cell'>
-            <span>
-              Genre : 
-            </span>
-
-            <span className={this.state.edit ? 'notDisplayed' : 'display'}>
-              {this.state.gender}
-            </span>
-
-            <input className={this.state.edit ? 'display' : 'notDisplayed'}/>
-          </div>
+          {this.items.map(item => {
+             return (<HeaderItem title={item}></HeaderItem>)
+          })}
         </div>
-
-        <button
-          className='editButton'
-          onClick={this.editHeader}
-        >edit</button>
       </div>
     )
   }
