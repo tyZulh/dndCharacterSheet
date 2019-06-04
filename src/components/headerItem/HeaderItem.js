d1a11547960ccbbcc1e71bed0591e63bd97e1a4e
Thomas Paoli <thomas@blueanchor.io>
2019-06-04 11:56:25 +0200

    add switch between input and display

--- src/components/headerItem/HeaderItem.js
+++ src/components/headerItem/HeaderItem.js
@@ -3,15 +3,37 @@ import './HeaderItem.scss';
 
 class HeaderItem extends Component {
   state = {
-    value : 'poil',
-    edit : true
+    edit : false,
+    headerItemData : {},
+  }
+  
+  headerItemData = {
+    properties : this.props.title,
+    value : ''
   }
 
-  displayEdit = _=> {
+  componentDidMount() {
+    if (!this.state.headerItemData.value) {
+      this.setState({ edit : true })
+    }
+    console.log(this.state);       
+  }
+
+  handleChange = event => {
+    this.setState({value : event.target.value})
+    this.headerItemData.value = event.target.value
+    this.setState({headerItemData : this.headerItemData})
+    console.log(this.state);
+    
+  }
+
+  displayEdit = _ => {
+    console.log(this.state);
+    
     this.setState({edit : !this.state.edit})
   }
 
-  render(){
+  render() {
     return(
       <div className='cell'>
         <span>
@@ -19,7 +41,7 @@ class HeaderItem extends Component {
         </span>
 
         <span 
-          className={this.state.edit ? 'notDisplayed' : 'display'}
+          // className={this.state.edit ? 'notDisplayed' : 'display'}
           onClick={this.displayEdit}
         >
           {this.state.value}
@@ -27,7 +49,7 @@ class HeaderItem extends Component {
 
         <input
           className={this.state.edit ? 'display' : 'notDisplayed'}
-          onSubmit={this.displayEdit}
+          onChange={this.handleChange}
           placeholder={this.state.value}
         />
         <button 
