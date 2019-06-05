6d1ba0d72b392786c87d27960faa04aa945e723d
Thomas Paoli <thomas@blueanchor.io>
2019-06-05 11:30:20 +0200

    add the characteristic-box to the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
+import CharactirsitcBox from './components/characteristics-box/Characteristics-box';
 import './App.scss';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <CharactirsitcBox/>
         
       </div>
     );
