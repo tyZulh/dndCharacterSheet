cf64319f2510c4b659b6094f4b180df26ca980d3
Thomas Paoli <thomas@blueanchor.io>
2019-06-13 15:49:05 +0200

    add the line for the ability

--- src/components/competence-line/CompetenceLine.js
+++ src/components/competence-line/CompetenceLine.js
@@ -2,10 +2,52 @@ import React, { Component } from 'react';
 import './CompetenceLine.scss';
 
 class CompetenceLine extends Component {
+  state = {
+    totalValue : 0,
+
+  }
+
   render() {
     return(
       <div className='competenceLineWrapper'>
-      
+        <div className='competenceLine'>
+          <div className='abilityName'>
+            {this.props.data}
+          </div>
+
+          <div className='modAbility cell'>
+            <input
+              type= "number"
+              name="modAbility"
+              className="inputBox"
+              onCHange={this.handleChange}
+            />
+          </div>
+
+          <div className='cell'>
+            <input
+              type="number"
+              name="master"
+              onCHange={this.handleChange}
+            />
+          </div>
+
+          <div className='cell'>
+            <input
+              type="number"
+              name="modCar"
+              onCHange={this.handleChange}
+            />
+          </div>
+
+          <div className='cell'>
+            <input
+              type="number"
+              name="otherMod"
+              onCHange={this.handleChange}
+            />
+          </div>
+        </div>
       </div>
     )
   }
