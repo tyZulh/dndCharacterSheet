b713d2df6785859ae2a93ba0173cd20fa52f1f18
Thomas Paoli <thomas@blueanchor.io>
2019-06-17 17:03:29 +0200

    fix of some import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/header/Header';
 import CharactirsitcBox from './components/characteristics-box/Characteristics-box';
 import './App.scss';
-import CompetencesBox from './components/competences-box/Compretences-box';
+import CompetencesBox from './components/competences-box/Competences-box';
 import SaveRollsBox from './components/saveRolls-box/SaveRolls-box';
 
 class App extends Component {
