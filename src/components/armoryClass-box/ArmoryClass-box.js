f55f8967bdfe3b3c0c51f4e9b2e7cb3a0cc22a51
Thomas Paoli <thomas@blueanchor.io>
2019-06-19 10:09:36 +0200

    create the armoryClass-box component

---
+++ src/components/armoryClass-box/ArmoryClass-box.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './ArmoryClass-box.scss'
+
+class ArmoryClassBox extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default ArmoryClassBox
