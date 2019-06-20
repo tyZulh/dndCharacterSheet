33d3f57c39a4c54b0a8dbccc35c7219be7ff4988
Thomas Paoli <thomas@blueanchor.io>
2019-06-20 15:37:51 +0200

    add minimalistic style and the calculation of the CAC and SAC

--- src/components/armoryClass-box/ArmoryClass-box.js
+++ src/components/armoryClass-box/ArmoryClass-box.js
@@ -16,18 +16,40 @@ class ArmoryClassBox extends Component {
   }
 
   totalValue = 0
+  contactClass = 0
+  surprisedClass = 0
 
   calculateClassArmory = () => {
     this.totalValue = 10 + this.state.armorBaseValue + this.state.dexModifier + this.state.shieldModifier + this.state.heightModifier + this.state.naturalArmorModifier + this.state.parryModifier + this.state.otherModifier
+    
     return this.totalValue
+  }
+
+  calculateContact = () => {
+    this.contactCLass = 10 + this.state.dexModifier + this.state.heightModifier
+    
+    return this.contactCLass
+  }
+
+  calculateSurprised = () => {
+    if (this.state.dexModifier < 0) {
+      this.surprisedClass = this.state.armoryClass
+    } else {
+      this.surprisedClass = this.state.armoryClass - this.state.dexModifier
+    }
+
+    return this.surprisedClass
   }
 
   handleChange = (event) => {
     this.setState({[event.target.name] : +event.target.value}, () => {
       this.calculateClassArmory()
       this.setState({armoryClass : this.totalValue}, ()=>{
-        console.log(this.state);
-        
+        this.calculateContact()
+        this.calculateSurprised()
+        this.setState({contactArmorClass : this.contactCLass, surprisedArmorClass : this.surprisedClass}, ()=>{
+          console.log(this.state);
+        })
       })
     })
   }
@@ -35,7 +57,6 @@ class ArmoryClassBox extends Component {
   render() {
     return (
       <div className="armoryClassWrapper">
-
         <section className="armorClassDetail">
 
           <div className="armorValue">
@@ -44,12 +65,14 @@ class ArmoryClassBox extends Component {
             </div>
             
             <div>
-              {this.state.armoryClass}
-            </div>
+              <div>
+                {this.state.armoryClass}
+              </div>
 
-            <span>
-              = 10 +
-            </span>
+              <span>
+                = 10 +
+              </span>
+            </div>
           </div>
 
           <div className="armorBaseValue cell">
@@ -57,16 +80,18 @@ class ArmoryClassBox extends Component {
               Bonus d'armure
             </label>
 
-            <input
-              type="number"
-              name="armorBaseValue"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
+            <div className="inputZone">
+              <input
+                type="number"
+                name="armorBaseValue"
+                onChange={this.handleChange}
+                className="inputBox"
+              />
 
-            <span>
-              +
-            </span>
+              <span>
+                +
+              </span>
+            </div>
 
           </div>
 
@@ -75,16 +100,18 @@ class ArmoryClassBox extends Component {
               Mod de dex
             </label>
 
-            <input
-              type="number"
-              name="dexModifier"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
+            <div className="inputZone">
+              <input
+                type="number"
+                name="dexModifier"
+                onChange={this.handleChange}
+                className="inputBox"
+                />
 
-            <span>
-              +
-            </span>
+              <span>
+                +
+              </span>
+            </div>
           </div>
 
           <div className="shieldModifier cell">
@@ -92,16 +119,18 @@ class ArmoryClassBox extends Component {
               Bonus de bouclier
             </label>
 
-            <input
-              type="number"
-              name="shieldModifier"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
+            <div className="inputZone">
+              <input
+                type="number"
+                name="shieldModifier"
+                onChange={this.handleChange}
+                className="inputBox"
+              />
 
-            <span>
-                +
-            </span>
+              <span>
+                  +
+              </span>
+            </div>
           </div>
 
           <div className="heightModifier cell">
@@ -109,33 +138,37 @@ class ArmoryClassBox extends Component {
               Mod de taille
             </label>
             
-            <input
-              type="number"
-              name="heightModifier"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
+            <div className="inputZone">
+              <input
+                type="number"
+                name="heightModifier"
+                onChange={this.handleChange}
+                className="inputBox"
+              />
 
-            <span>
-              +
-            </span>
+              <span>
+                +
+              </span>
+            </div>
           </div>
 
           <div className="naturalArmorModifer cell">
             <label className="naturalArmorModifierHeader">
               Bonus d'armure naturelle
             </label>
-            
-            <input
-              type="number"
-              name="naturalArmorModifier"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
 
-            <span>
-              +
-            </span>
+            <div className="inputZone">
+              <input
+                type="number"
+                name="naturalArmorModifier"
+                onChange={this.handleChange}
+                className="inputBox"
+                />
+
+              <span>
+                +
+              </span>
+            </div>
           </div>
 
           <div className="parryModifier cell">
@@ -143,23 +176,25 @@ class ArmoryClassBox extends Component {
               Bonus de parrade
             </label>
 
-            <input
-              type="number"
-              name="parryModifier"
-              onChange={this.handleChange}
-              className="inputBox"
-            />
+            <div className="inputZone">
+              <input
+                type="number"
+                name="parryModifier"
+                onChange={this.handleChange}
+                className="inputBox"
+                />
 
-            <span>
-              +
-            </span>
+              <span>
+                +
+              </span>
+            </div>
           </div>
 
           <div className="otherModifier cell">
             <label className="otherModifierHeader">
               Modificateur divers
             </label>
-
+            
             <input
               type="number"
               name="otherModifier"
@@ -167,6 +202,26 @@ class ArmoryClassBox extends Component {
               className="inputBox"
             />
           </div>  
+        </section>
+        
+        <section className="contactArmorClass">
+          <span className="cona">
+            CA de contact 
+          </span>
+
+          <span>
+            {this.state.contactArmorClass}
+          </span>
+        </section>
+
+        <section>
+          <span className="surprisedArmorClass">
+            CA pris au dépourvu 
+          </span>
+
+          <span>
+            {this.state.surprisedArmorClass}
+          </span>
         </section>
       </div>
     )
