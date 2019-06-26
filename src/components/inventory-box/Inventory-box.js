4cb9cd0fbdfa375e4873a739d9981bbc2d0bc6ad
Thomas Paoli <thomas@blueanchor.io>
2019-06-26 11:52:07 +0200

    add header to the inventory

--- src/components/inventory-box/Inventory-box.js
+++ src/components/inventory-box/Inventory-box.js
@@ -55,11 +55,30 @@ class InventoryBox extends Component {
   render() {
     return(
       <section className="inventoryBox">
-        <div className="addEquipement">
-          <span>
-            Equipement : 
-          </span>
 
+        <div className="inventoryHeader">
+          <div>
+            Equipement
+          </div>
+
+          <div>
+            Note
+          </div>
+
+          <div>
+            Emplacement
+          </div>
+
+          <div>
+            Poid
+          </div>
+
+          <div>
+            Prix
+          </div>
+        </div>
+
+        <div className="addEquipement">
           <div className="">
             <input
               className="inputBox"
@@ -76,7 +95,7 @@ class InventoryBox extends Component {
               type="textarea"
               name="equipementToAddNote"
               onChange={this.handleChange}
-              value={this.state.equipementToAddName}
+              value={this.state.equipementToAddNote}
               />
           </div>
 
@@ -110,9 +129,9 @@ class InventoryBox extends Component {
             />
           </div>
 
-          <div className="addButton" onClick={this.addEquipement}>
-            Ajouter
-          </div>
+          <button className="addButton" onClick={this.addEquipement}>
+            +
+          </button>
         </div>
       
         <div className="equipementList">
@@ -121,24 +140,24 @@ class InventoryBox extends Component {
             
             
             return (
-              <div>
-                <div>
+              <div className="equipementListLine">
+                <div className="equipementCell">
                   {equipement.name}
                 </div>
 
-                <div>
+                <div className="equipementCell">
                   {equipement.note}
                 </div>
 
-                <div>
+                <div className="equipementCell">
                   {equipement.location}
                 </div>
 
-                <div>
+                <div className="equipementCell">
                   {equipement.price}
                 </div>
 
-                <div>
+                <div className="equipementCell">
                   {equipement.weight}
                 </div>
               </div>
