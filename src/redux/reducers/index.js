70223b881dfa2efc9872ae6aefea4ef7459eb09a
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-10-09 18:00:59 +0200

    modify the reducer to have a true immutable store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,16 @@
+import { ADD_CHARACTERISTICS } from "../constants/action-types"
+
 const initialState = {
-  characteristics: []
+  characteristics: {}
 };
 
 const rootReducer = (state = initialState, action) => {
+  if (action.types === ADD_CHARACTERISTICS) {
+    const charSelector = action.payload.charSelector
+    return Object.assign({}, state, {
+      characteristics: state.characteristics[charSelector](action.payload.charValue)
+    })
+  }
   return state;
 };
 
