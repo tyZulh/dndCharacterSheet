c2859e55218df07daaf2948f47cc64704842c1b8
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-10-16 16:01:17 +0200

    add routes for the characteristics

--- back/index.js
+++ back/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./Routes/index')
+
 const app = express();
 
+
 const port = 4000;
+
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
+console.log(routes)
+
+app.use("/characteristics", routes.Characteristics)
+
+app.get('/', (req, res) => {
+  res.status(200).send('stay a while and listen')
+})
 
 app.listen(port, (err) => {
   if(err) {
@@ -9,3 +31,4 @@ app.listen(port, (err) => {
   }
   console.log(`Server is listening on ${port}`)
 })
+
